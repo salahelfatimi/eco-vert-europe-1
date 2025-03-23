06dd6b230337bf46c162ea3b1c107ca76a5d5952
salahelfatimi <salahfatimi16@gmail.com>
2025-03-23 21:39:58 +0000

    Update service page layout for improved text alignment and styling

--- src/app/nos_service/page.jsx
+++ src/app/nos_service/page.jsx
@@ -10,7 +10,7 @@ export default function Service(){
                 <div className=" absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-10"></div>
                 <Image src="/img/img1.jpg" alt="Eco Vert Europé" title="Eco Vert Europé" width={1920} loading="eager" height={1080} className="  shadow-2xl header-image object-cover h-screen w-screen object-center" />
                 <div className=" absolute inset-0 flex flex-col items-center justify-center z-20">
-                    <h1 className=" tex-4xl lg:text-8xl text-white font-panchang-bold"> Nos Service</h1>
+                    <h1 className=" text-4xl text-center lg:text-8xl text-white font-panchang-bold"> Nos Service</h1>
                 </div>
             </div>
             <div className="  flex flex-col gap-4 ">
@@ -19,9 +19,9 @@ export default function Service(){
                         <div className="lg:w-1/2 lg:h-[30rem] ">
                             <Image width={1920} height={1080} src={service.img} className=" border-primary w-full h-full rounded-[30px]  border-4 object-center object-cover" alt="Eco Vert Europé" title="Eco Vert Europé"/>
                         </div>
-                        <div className="lg:w-1/2 flex flex-col  justify-center gap-6  ">
+                        <div className="lg:w-1/2 flex flex-col    justify-center gap-6  ">
                             <div className=" flex items-center gap-4 ">
-                                <MoveUpRight className="stroke-white stroke-2 " size={50} />
+                                <MoveUpRight className="stroke-primary stroke-2 min-w-10  " size={50} />
                                 <h3 className="font-panchang-bold  font-semibold text-primary text-lg lg:text-2xl uppercase">{service.title}</h3>
                             </div>
                             <p className=" font-panchang text-lg  text-secondary  ">{service.description}</p>
