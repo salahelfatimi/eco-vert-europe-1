eb847c0665c0b66a540a4b4cd53e373136bfb4b3
salahelfatimi <salahfatimi16@gmail.com>
2025-04-20 16:06:21 +0100

    add

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,7 @@
 export default function Loading() {
   return (
     <>
-      <div
-        className={`fixed  inset-0 flex items-center justify-center   duration-1000 transition z-50  min-h-screen `}
-      >
+      <div className={`fixed  inset-0 flex items-center justify-center   duration-1000 transition z-50  min-h-screen `}>
         <span className={`font-extrabold text-center items-center gap-3 md:text-5xl text-4xl animate-bounce flex flex-col lg:flex-row justify-center  `}>
           Bienvenue à
           <span className=" text-primary   ">
