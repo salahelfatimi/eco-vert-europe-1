5eb3ebc3533aae14e7b9fb749b1e74fce4c6ad4e
salahelfatimi <salahfatimi16@gmail.com>
2025-04-20 16:20:42 +0100

    Remove old sitemap route and add new sitemap.js for improved URL handling

---
+++ src/app/sitemap.js
@@ -0,0 +1,77 @@
+export default function sitemap() {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/a_propos`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/reserver_un_devis`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/amelioration_energetique`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/carrelage`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/chauffage_pompe_a_chaleur`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/construction_amenagement`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/creation_salle_de_bain`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/electricite`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/etancheite`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/facade_isolation`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/plaque_de_platre_cloisons`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/plomberie`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/renovation_complete`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/revetements_de_sols_en_resine`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/toiture`,
+      lastModified: new Date(),
+    },
+  ];
+}
+e
