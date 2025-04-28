cfbfc4c7d2084374a15b223b9bae1311c1a04160
salahelfatimi <salahfatimi16@gmail.com>
2025-04-28 21:47:46 +0100

    Refactor cookie consent message layout for improved alignment and readability

--- src/components/tools/cookies/message.jsx
+++ src/components/tools/cookies/message.jsx
@@ -34,9 +34,9 @@ export default function MessageCookies() {
       <>
         {isVisible && (
             <div className="fixed bottom-4 left-4 font-panchang flex items-center justify-center text-white p-4 z-50">
-            <div className="container flex flex-col items-start mx-auto  bg-secondary rounded-lg shadow-lg p-6"> 
-              <p className=" font-bold text-xl text-start ">cookies consentement</p>
-              <p className="text-xs text-start mb-4"> Ce site utilise des cookies pour améliorer votre expérience. Consultez notre{" "} <Link href="/politique-de-confidentialite" className="text-primary underline hover:text-primary" > politique de confidentialité </Link> . </p>
+            <div className="container flex flex-col items-center lg:items-start mx-auto  bg-secondary rounded-lg shadow-lg p-6"> 
+              <p className=" font-bold  text-center ">cookies consentement</p>
+              <p className="text-xs text-center mb-4"> Ce site utilise des cookies pour améliorer votre expérience. Consultez notre{" "} <Link href="/politique-de-confidentialite" className="text-primary underline hover:text-primary" > politique de confidentialité </Link> . </p>
                 <div className="flex gap-4 justify-start">
                     <button className="bg-primary text-gray-900 font-semibold py-2 px-4 rounded hover:bg-transparent border-2 border-primary hover:text-primary transition text-sm" onClick={() => handleCookiesDecision(true)} > Accepter </button>
                     <button className="bg-gray-600 text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition text-sm" onClick={() => handleCookiesDecision(false)} > Refuser </button>
